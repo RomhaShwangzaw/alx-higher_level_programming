37af209349fcdbf92231d5409cc39bb249bfc5b4
Romha Shwangzaw <romhas@gmail.com>
2022-09-20 04:52:40 -0700

    last_digit script added

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -5,7 +5,7 @@ str = "Last digit of {} is {} {}"
 last = number % 10
 if last > 5:
     print(str.format(number, last, "and is greater than 5"))
-elif last = 0:
+elif last == 0:
     print(str.format(number, last, "and is 0"))
 else:
     print(str.format(number, last, "and is less than 6 and not 0"))
