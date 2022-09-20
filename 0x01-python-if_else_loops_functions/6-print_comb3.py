24c0fbe1e102345ab59e6c90591bd14af63bd530
Romha Shwangzaw <romhas@gmail.com>
2022-09-20 05:51:57 -0700

    pycodestyle fix

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 for i in range(10):
-        for j in range(i + 1, 10):
-            print("{}".format(i), end="")
-            if i < 8:
-                print("{}".format(j), end=", ")
-            else:
-                print("{}".format(j))
+    for j in range(i + 1, 10):
+        print("{}".format(i), end="")
+        if i < 8:
+            print("{}".format(j), end=", ")
+        else:
+            print("{}".format(j))
