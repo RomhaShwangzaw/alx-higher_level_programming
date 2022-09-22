088ebcaeee5329ab0b27a32e400a24cb74ca295d
Romha Shwangzaw <romhas@gmail.com>
2022-09-22 02:29:41 -0700

    infinite addition of arguments done

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import sys
+
+if __name__ == "__main__":
+    i = 0
+    res = 0
+    for arg in sys.argv:
+        if i != 0:
+           res += int(arg)
+        i = 1
+
+    print(res)
