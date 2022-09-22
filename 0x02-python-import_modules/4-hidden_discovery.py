f5cad49ba8f348b4edf67be84931185f760adba7
Romha Shwangzaw <romhas@gmail.com>
2022-09-22 02:42:49 -0700

    pycodestyle fix

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -4,5 +4,5 @@ import hidden_4
 if __name__ == "__main__":
     names = dir(hidden_4)
     for name in names:
-        if name.startswith('__') == False:
+        if name.startswith('__') is False:
             print(name)
