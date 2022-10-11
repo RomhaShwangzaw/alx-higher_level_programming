563beeaae2410f09092ab9775aa852c9b215e36b
Romha Shwangzaw <romhas@gmail.com>
2022-10-11 12:27:27 -0700

    size attribute added

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+''' A module containing a Square class '''
+
+
+class Square:
+    ''' A Square class'''
+
+    def __init__(self, size=0):
+        ''' Initialization function '''
+        self.__size = size
