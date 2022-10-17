3268b69ea3982a3782f9ea8852c151167447f529
Romha Shwangzaw <romhas@gmail.com>
2022-10-17 03:00:24 -0700

    text_indentation module and doctest files added

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+''' THIS MODULE INDENTS A TEXT BASED ON A FEW SPECIAL CHARACTERS '''
+
+
+def text_indentation(text):
+    ''' prints a text with 2 new lines after each of these characters:
+        ., ? and :
+    '''
+
+    if not isinstance(text, str):
+        raise TypeError('text must be a string')
+
+    c = 0
+    while c < len(text) and text[c] == ' ':
+        c += 1
+
+    while c < len(text):
+        print(text[c], end="")
+        if text[c] == "\n" or text[c] in ".?:":
+            if text[c] in ".?:":
+                print("\n")
+            c += 1
+            while c < len(text) and text[c] == ' ':
+                c += 1
+            continue
+        c += 1
