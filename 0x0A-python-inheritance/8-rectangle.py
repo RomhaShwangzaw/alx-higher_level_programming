c4e2ec7ce9f182a5a4bf63270a74473c34e16a8e
Romha Shwangzaw <romhas@gmail.com>
2022-10-24 09:47:33 -0700

    Bug fix

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -9,7 +9,7 @@ class Rectangle(BaseGeometry):
 
     def __init__(self, width, height):
         '''Initialization method'''
-        integer_validator(self, "width", width)
-        integer_validator(self, "height", height)
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
         self.__width = width
         self.__height = height
