4196f90402b9becee9198f87d852616f6dca32d1
Romha Shwangzaw <romhas@gmail.com>
2022-10-25 10:05:58 -0700

    Style fix

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -10,13 +10,13 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
 if os.path.isfile('add_item.json'):
-	with open('add_item.json', encoding='utf-8') as f:
-		my_list = json.load(f)
+    with open('add_item.json', encoding='utf-8') as f:
+        my_list = json.load(f)
 else:
-	my_list = []
+    my_list = []
 
 for i in sys.argv[1:]:
-	my_list.append(i)
+    my_list.append(i)
 
 with open('add_item.json', 'w', encoding='utf-8') as f:
-	json.dump(my_list, f)
+    json.dump(my_list, f)
