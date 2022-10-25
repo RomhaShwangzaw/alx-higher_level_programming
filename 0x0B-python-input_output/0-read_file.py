154319417f8753918a19f6fb1c32e3ee1a9e1d1f
Romha Shwangzaw <romhas@gmail.com>
2022-10-25 08:03:27 -0700

    read_file function added

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+'''Defines how to read a file and print its contents'''
+
+
+def read_file(filename=""):
+    '''reads a text file (UTF8) and prints it to stdout.'''
+    with open(filename, encoding='utf-8') as f:
+        print(f.read())
