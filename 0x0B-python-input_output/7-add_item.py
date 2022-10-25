65b7db8dadd007f2ee715a07939a5f374fc340f8
Romha Shwangzaw <romhas@gmail.com>
2022-10-25 16:58:40 +0000

    add_item script added

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+'''Defines a script that adds all arguments to a Python list,
+and then save them to a file.
+'''
+import json
+import sys
+import os
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+
+if os.path.isfile('add_item.json'):
+	with open('add_item.json', encoding='utf-8') as f:
+		my_list = json.load(f)
+else:
+	my_list = []
+
+for i in sys.argv[1:]:
+	my_list.append(i)
+
+with open('add_item.json', 'w', encoding='utf-8') as f:
+	json.dump(my_list, f)
