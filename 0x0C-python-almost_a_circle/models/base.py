eec5d571e2d57a2c010995d8137cf00f3ce85d1a
Romha Shwangzaw <romhas@gmail.com>
2022-10-30 05:57:36 -0700

    task 15 done

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 '''Defines a Base class for all other classes'''
+import json
 
 
 class Base:
@@ -15,3 +16,17 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        '''returns the JSON string representation of list_dictionaries:
+
+        - list_dictionaries is a list of dictionaries
+        - If list_dictionaries is None or empty, return the string: "[]"
+        - Otherwise, return the JSON string representation of list_dictionaries
+
+        '''
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+
+        return json.dumps(list_dictionaries)
