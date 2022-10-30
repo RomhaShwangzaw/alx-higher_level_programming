af3826b4c0c9a162825676d78125da9266d59a6c
Romha Shwangzaw <romhas@gmail.com>
2022-10-30 04:50:29 -0700

    task 11 done

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -8,6 +8,19 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         '''Initialization method'''
         super().__init__(size, size, x, y, id)
+        self.size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        if type(size) != int:
+            raise TypeError('width must be an integer')
+        if size <= 0:
+            raise ValueError('width must be > 0')
+        self.__size = self.width = self.height = size
 
     def __str__(self):
         '''returns a string representation of a Square instance.'''
