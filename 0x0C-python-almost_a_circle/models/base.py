54a6d2ce270469d0cf949cc6285fcde5622866ea
Romha Shwangzaw <romhas@gmail.com>
2022-10-30 06:34:17 -0700

    task 17 done

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -52,3 +52,17 @@ class Base:
         with open(filename, 'w', encoding='utf-8') as f:
             json_str = Base.to_json_string(list_dictionaries)
             f.write(json_str)
+
+    @staticmethod
+    def from_json_string(json_string):
+        '''returns the list of the JSON string representation json_string:
+
+        - json_string is a string representing a list of dictionaries
+        - If json_string is None or empty, return an empty list
+        - Otherwise, return the list represented by json_string
+
+        '''
+        if json_string is None or len(json_string) == 0:
+            return []
+
+        return json.loads(json_string)
