f1e8e22a3da876c164e0c4b610e151173286bc95
Romha Shwangzaw <romhas@gmail.com>
2022-10-30 06:27:04 -0700

    task 16 done

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -30,3 +30,25 @@ class Base:
             return "[]"
 
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        '''writes the JSON string representation of list_objs to a file:
+
+        - list_objs is a list of instances who inherits of Base - example:
+                            list of Rectangle or list of Square instances
+        - If list_objs is None, save an empty list
+        - The filename must be: <Class name>.json - example: Rectangle.json
+        - You must use the static method to_json_string (created before)
+        - You must overwrite the file if it already exists
+
+        '''
+        filename = cls.__name__ + ".json"
+        list_dictionaries = []
+        if list_objs is not None:
+            for obj in list_objs:
+                list_dictionaries.append(obj.to_dictionary())
+
+        with open(filename, 'w', encoding='utf-8') as f:
+            json_str = Base.to_json_string(list_dictionaries)
+            f.write(json_str)
