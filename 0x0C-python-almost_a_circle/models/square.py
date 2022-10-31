c9f6beeea49304c105db7bc0d1f87fe3376d8265
Romha Shwangzaw <romhas@gmail.com>
2022-10-31 10:37:21 -0700

    Bug fix

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -8,19 +8,15 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         '''Initialization method'''
         super().__init__(size, size, x, y, id)
-        self.size = size
 
     @property
     def size(self):
-        return self.__size
+        return self.width
 
     @size.setter
     def size(self, size):
-        if type(size) != int:
-            raise TypeError('width must be an integer')
-        if size <= 0:
-            raise ValueError('width must be > 0')
-        self.__size = self.width = self.height = size
+        self.width = size
+        self.height = size
 
     def __str__(self):
         '''returns a string representation of a Square instance.'''
