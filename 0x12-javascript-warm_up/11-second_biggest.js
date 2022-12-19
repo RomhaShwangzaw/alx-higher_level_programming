c82f033460716ac8243870720eca5dd290382d7f
Romha Shwangzaw <romhas@gmail.com>
2022-12-19 20:00:52 +0300

    Bug Fix

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,13 +1,9 @@
 #!/usr/bin/node
-const args = process.argv;
-if (args.length < 4) {
+if (process.argv.length <= 3) {
   console.log(0);
 } else {
-  const myArray = [];
-  for (let i = 2; i < args.length; i++) {
-    myArray.push(parseInt(args[i], 10));
-  }
-  myArray.sort();
-  myArray.reverse();
-  console.log(myArray[1]);
+  const args = process.argv.map(Number)
+    .slice(2, process.argv.length)
+    .sort((a, b) => a - b);
+  console.log(args[args.length - 2]);
 }
