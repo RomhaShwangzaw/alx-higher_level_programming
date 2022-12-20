985897406e705fd78560c5e96882f9647b25595e
Romha Shwangzaw <romhas@gmail.com>
2022-12-20 13:45:14 +0300

    charPrint method added to Square class

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+module.exports = class Square extends require('./5-square') {
+  charPrint (c) {
+    if (!c) this.print();
+    else {
+      for (let i = 0; i < this.height; i++) {
+        let row = '';
+        for (let j = 0; j < this.width; j++) row += c;
+        console.log(row);
+      }
+    }
+  }
+};
