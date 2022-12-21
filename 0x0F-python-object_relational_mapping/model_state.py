ab5145f1f4e9b3012130be8905a3e322b537e907
Romha Shwangzaw <romhas@gmail.com>
2022-12-21 18:26:17 +0300

    Bug Fix

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -16,6 +16,5 @@ class State(Base):
     name (sqlalchemy.String): The state's name.
     """
     __tablename__ = "states"
-
     id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
