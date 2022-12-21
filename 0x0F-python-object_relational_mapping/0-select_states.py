22bfcf55e9a30b46777f473235169ae99dd4539b
Romha Shwangzaw <romhas@gmail.com>
2022-12-21 12:45:06 +0300

    Used MySQLdb module to perform a SQL query

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" This module lists all states from the database hbtn_0e_0_usa """
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                           passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    query_rows = cur.fetchall()
+    for id, name in query_rows:
+        print('({}, {})'.format(id, name))
+    cur.close()
+    conn.close()
