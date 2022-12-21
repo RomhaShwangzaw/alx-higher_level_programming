51ce48e9ca4da315a31567bf96f34a1876d30e9d
Romha Shwangzaw <romhas@gmail.com>
2022-12-21 13:20:28 +0300

    Case-sensitivity bug fixed

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                            passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
     cur = conn.cursor()
-    cur.execute("SELECT * FROM states WHERE name='{}' ORDER BY id"
+    cur.execute("SELECT * FROM states WHERE BINARY name='{}' ORDER BY id"
                 .format(sys.argv[4]))
     query_rows = cur.fetchall()
     for state in query_rows:
