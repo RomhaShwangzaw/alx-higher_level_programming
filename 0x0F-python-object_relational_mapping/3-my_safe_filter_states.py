3871b7a166665eff1d4e6a27f2ef8fe30be84e4d
Romha Shwangzaw <romhas@gmail.com>
2022-12-21 14:00:52 +0300

    safe_filter from sql injection added

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" This module takes in an argument and displays all values in the states
+table of hbtn_0e_0_usa where name matches the argument.
+"""
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                           passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
+    cur = conn.cursor()
+    searched = sys.argv[4]
+    cur.execute("""SELECT * FROM states WHERE BINARY name = %(state_name)s
+                ORDER BY id""", {'state_name': searched})
+    query_rows = cur.fetchall()
+    for state in query_rows:
+        print(state)
+    cur.close()
+    conn.close()
