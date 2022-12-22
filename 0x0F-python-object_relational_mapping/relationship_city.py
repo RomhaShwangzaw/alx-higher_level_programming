25da5a7a3eb3b2bfcbaa056b2db26fe844ec8f07
Romha Shwangzaw <romhas@gmail.com>
2022-12-22 15:43:06 +0300

    Minor Fix

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -1,26 +1,23 @@
 #!/usr/bin/python3
 """
-This script defines a City class that inherits from
-Base class to work with MySQLAlchemy ORM.
+This script defines a City class
+to work with MySQLAlchemy ORM.
 """
 
-from relationship_state import Base
-from sqlalchemy import Column, ForeignKey, Integer, String
-from sqlalchemy.orm import relationship
+from relationship_state import Base, State
+from sqlalchemy import Column, Integer, String, ForeignKey
 
 
 class City(Base):
     """City class
     Attributes:
         __tablename__ (str): The table name of the class
-        id (int): The City id of the class
-        name (str): The City name of the class
-        state_id (int): The State id of the class
+        id (int): The id of the class
+        name (str): The name of the class
+        state_id (int): The state the city belongs to
     """
     __tablename__ = 'cities'
 
     id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey('states.id'))
-
-    state = relationship("State", back_populates="cities")
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -5,8 +5,8 @@ a Base class to work with MySQLAlchemy ORM.
 """
 
 from sqlalchemy import Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
@@ -17,10 +17,10 @@ class State(Base):
         __tablename__ (str): The table name of the class
         id (int): The State id of the class
         name (str): The State name of the class
+        cities (:obj:`City`): The Cities belongs to State
     """
     __tablename__ = 'states'
 
     id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
-
-    cities = relationship("City", back_populates="state")
+    cities = relationship("City", backref="state", cascade="all, delete")
