7c804010b260448934255ec1c6ccd280e702fc89
Romha Shwangzaw <romhas@gmail.com>
2022-12-22 14:21:54 +0300

    Bug Fix

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-"""
-This script lists all State objects, and corresponding City objects,
-contained in the database hbtn_0e_101_usa
-"""
-
+# Lists all States and corresponding Cities in the database hbtn_0e_101_usa.
+# Usage: ./101-relationship_states_cities_list.py <mysql username> /
+#                                                 <mysql password> /
+#                                                 <database name>
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -21,4 +20,3 @@ if __name__ == "__main__":
         print("{}: {}".format(state.id, state.name))
         for city in state.cities:
             print("    {}: {}".format(city.id, city.name))
-    session.close()
