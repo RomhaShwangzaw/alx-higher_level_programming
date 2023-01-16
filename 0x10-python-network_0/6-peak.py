818b659feeb70e6cbe0e60ab7369d3e20bd41774
Romha Shwangzaw <romhas@gmail.com>
2023-01-17 01:17:35 +0300

    Bug Fix

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -3,7 +3,20 @@
 
 def find_peak(list_of_integers):
     """Finds the peak of a list of integers"""
-    if len(list_of_integers) == 0:
+    if list_of_integers == []:
         return None
-    list_of_integers.sort()
-    return list_of_integers[len(list_of_integers) - 1]
+
+    size = len(list_of_integers)
+    if size == 1:
+        return list_of_integers[0]
+    elif size == 2:
+        return max(list_of_integers)
+
+    mid = int(size / 2)
+    peak = list_of_integers[mid]
+    if peak > list_of_integers[mid - 1] and peak > list_of_integers[mid + 1]:
+        return peak
+    elif peak < list_of_integers[mid - 1]:
+        return find_peak(list_of_integers[:mid])
+    else:
+        return find_peak(list_of_integers[mid + 1:])
