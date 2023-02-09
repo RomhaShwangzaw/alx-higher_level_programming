e97653056e56e182c2987326e24468454516d037
Romha Shwangzaw <romhas@gmail.com>
2023-02-09 22:01:28 +0300

    Bug Fix

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -7,24 +7,12 @@ request(url, (error, response, body) => {
   // Printing the error if occurred
   if (error) console.log(`code: ${error}`);
 
-  const result = {
-    1: 0,
-    2: 0,
-    3: 0,
-    4: 0,
-    5: 0,
-    6: 0,
-    7: 0,
-    8: 0,
-    9: 0,
-    10: 0
-  };
+  const result = {};
   const tasks = JSON.parse(body);
   for (const task of tasks) {
     if (task.completed) {
-      // if (result.get(task.userId)) result[task.userId] += 1;
-      // else result[task.userId] = 1;
-      result[task.userId] += 1;
+      if (result[task.userId] === undefined) result[task.userId] = 1;
+      else result[task.userId]++;
     }
   }
 
